45c46b21e5bb1880f9e8d0a464d4f58b3d38992b
root <root@localhost.localdomain>
2016-07-16 00:09:12 +0000

    updated kstart

--- kstart.sh
+++ kstart.sh
@@ -6,13 +6,10 @@ LIST_OF_APPS="tmux"
 apt-get update
 apt-get install emacs -y
 #Set up emacs correctly
-rm ~/.emacs
 echo "(custom-set-variables
  '(custom-enabled-themes (quote (manoj-dark)))
  '(inhibit-startup-screen t))
 (custom-set-faces)" > ~/.emacs
-# Add terminal shortcut to list of shortcuts
-sed -i '37 <property name="&lt;Primary&gt;&lt;Alt&gt;t" type="string" value="xfce4-terminal"/>' /root/.config/xfce4/xfconf/xfce-perchannel-xml
 apt-get install $LIST_OF_APPS -y
 # Move config files to correct location
 mv ~/Public/random_code/.tmux.conf ~
