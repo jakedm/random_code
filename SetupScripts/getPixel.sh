c1b202a7267a1c65e72426345b85cffa187d505b
jake <jake.d.moorhead@gmail.com>
2017-02-21 14:49:24 -0800

    Complete restructuring of git repo.

---
+++ SetupScripts/getPixel.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+FLAGS="-y --show-progress"
+GUI=( "mate-desktop-environment-core" "xfce4 xfce4-terminal" "raspberrypi-ui-mods" )
+DEFAULT=1
+
+echo Default GUI is ${GUI[$DEFAULT]}, reset script to change.
+sleep 5
+
+echo Installing Xorg:
+sudo apt-get $FLAGS xserver-xorg
+echo
+echo
+
+echo Installing Xinit:
+sudo apt-get install $FLAGS xinit
+echo
+echo
+
+echo Installing Xinit:
+sudo apt-get install $FLAGS xinit
+echo
+echo
+
+echo Installing ${GUI[$DEFAULT]}:
+sudo apt-get install $FLAGS ${GUI[$DEFAULT]}
+echo
+echo
+
+echo Installing lightdm:
+sudo apt-get install $FLAGS lightdm
